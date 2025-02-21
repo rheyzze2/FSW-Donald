2459d7fee9edebbc624b21169a31b82f50e41136
rheyzze2 <rheyzzer@gmail.com>
2025-02-20 21:13:05 -0300

    feat: add uniq constraint to restaurant slug

--- prisma/migrations/20250219235651_add_unique_on_slug/migration.sql
+++ prisma/migrations/20250219235651_add_unique_on_slug/migration.sql
@@ -1,7 +1,10 @@
 /*
   Warnings:
 
+  - You are about to drop the column `nome` on the `Restaurant` table. All the data in the column will be lost.
   - You are about to drop the `menuCategory` table. If the table is not empty, all the data it contains will be lost.
+  - A unique constraint covering the columns `[slug]` on the table `Restaurant` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `name` to the `Restaurant` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
@@ -12,6 +15,10 @@ ALTER TABLE "Product" DROP CONSTRAINT "Product_menuCategoryId_fkey";
 
 -- DropForeignKey
 ALTER TABLE "menuCategory" DROP CONSTRAINT "menuCategory_restaurantid_fkey";
+
+-- AlterTable
+ALTER TABLE "Restaurant" DROP COLUMN "nome",
+ADD COLUMN     "name" TEXT NOT NULL;
 
 -- DropTable
 DROP TABLE "menuCategory";
@@ -26,6 +33,9 @@ CREATE TABLE "MenuCategory" (
 
     CONSTRAINT "MenuCategory_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Restaurant_slug_key" ON "Restaurant"("slug");
 
 -- AddForeignKey
 ALTER TABLE "MenuCategory" ADD CONSTRAINT "MenuCategory_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ src/app/[slug]/components/index.ts
@@ -0,0 +1,1 @@
+export * from "./consumption-method-option";
